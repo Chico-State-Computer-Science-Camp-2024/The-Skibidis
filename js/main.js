92a4a35fa589b440fddc8f653d64b9ffbce334b3
orlandobedabest <thecreeperboi21@gmail.com>
2024-07-09 18:01:14 +0000

    started js

--- js/main.js
+++ js/main.js
@@ -1,5 +1,50 @@
-$( document ).ready(function() {
-    $('#btn').click(function() {
-        window.alert("DID YOU JUST CLICK ME?!");
-    });
-});
+let frog;
+let car;
+let log;
+
+function preload (){
+    img = loadImage('./images/skibidi.png');
+}
+function setup(){
+    createCanvas(500, 500);
+    xPos = 0;
+    yPos = 0;
+}
+function draw(){
+    line(4, 4, 4, 1000);
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*const map = [[1, 2, 3],
+             [4, 5 ,6], 
+             [7, 8, 9]];
+             ['*', 0, '#']
+
+/*map[0][0] = 'X';
+map[0][1] = 'O';
+map[0][2] = 'X';
+
+map[1][0] = 'O';
+map[1][1] = 'X';
+map[1][2] = 'O';
+
+map[2][0] = 'X';
+map[2][1] = 'O';
+map[2][2] = 'X';
+
+for(let row of map){
+    const rowString = row.join(' ');
+    console.log(rowString);
+}
