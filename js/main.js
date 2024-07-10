499290cdd96bdf0b96db4fbe8d5e848a1b47a8f0
orlandobedabest <thecreeperboi21@gmail.com>
2024-07-10 20:48:38 +0000

    returned the canvas

--- js/main.js
+++ js/main.js
@@ -3,11 +3,11 @@ let canvas = document.getElementById("canvas");
 
 let context = canvas.getContext("2d");
 
-var window_height = window.innerHeight;
-var window_width = window.innerWidth;
+let window_height = window.innerHeight;
+let window_width = window.innerWidth;
 
-canvas.width = "windowWidth";
-canvas.height = "580";
+canvas.width = window_width;
+canvas.height = window_height;
 canvas.style.background = "green";
 
 context.fillStyle = "gray";
