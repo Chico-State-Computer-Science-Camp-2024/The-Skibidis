7c9a5dc3c288cf5c0903f423a097751699b56907
Angel Urrea <urreaangel80@gmail.com>
2024-07-10 16:07:42 +0000

    Added movement controls that need to be added to skibidi

--- js/main.js
+++ js/main.js
@@ -1,5 +1,31 @@
-$( document ).ready(function() {
-    $('#btn').click(function() {
-        window.alert("DID YOU JUST CLICK ME?!");
-    });
+//const variableName = document.getElementById("variableName"); 
+const moveAmount = 10; 
+let x = 0;
+let y = 0;
+
+document.addEventListener("keydown", event => {
+    if(event.key.startsWith("Arrow")){
+        switch(event.key){
+            case "ArrowUp": 
+                 y -= moveAmount;
+                 break;
+             case "ArrowDown": 
+                 y += moveAmount;
+                break;
+            case "ArrowLeft": 
+                 x -= moveAmount;
+                 break;
+             case "ArrowRight": 
+                 x += moveAmount;
+                 break;
+        }
+        
+        //variableName.style.top = "${y}px";
+        //variableName.style.left = "${x}px";
+    }
 });
+
+document.addEventListener("keyup", event => {
+    console.log("Key up = ${event.key}");
+});
+ 
